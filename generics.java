3c84adf68581a185c37eaf2aedee39fd7cd48c40
Mifdha Milan <mifmilan@gmail.com>
2021-05-14 23:03:35 +0530

    completed

---
+++ generics.java
@@ -0,0 +1,16 @@
+//Question: https://www.hackerrank.com/challenges/30-generics/problem
+
+
+public static <E> void printArray(E[] inputArray){
+    for(E ele: inputArray){
+        System.out.println(ele);
+    }
+}
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+
